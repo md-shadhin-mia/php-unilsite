f24cf6a0485b1063ed226e417119295cf6e0379e
MD Shadhin <mdshadhinkn@gmail.com>
2023-11-10 00:17:33 +0600

    game file added

---
+++ game.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snake Game with Pixi.js</title>
+    <script src="https://cdn.jsdelivr.net/npm/pixi.js@6.x/dist/browser/pixi.min.js"></script>        <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f0f0f0;
+        }
+    </style>
+</head>
+<body>
+    <script src="game.js"></script>
+</body>
+</html>
