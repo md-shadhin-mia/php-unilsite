35ce1d9e044c97c5d1d9875972fd3e1d60411609
MD Shadhin <mdshadhinkn@gmail.com>
2024-09-16 00:19:33 +0600

    all aded

---
+++ commit.bat
@@ -0,0 +1,156 @@
+date 09-15-23
+git add vendor/masterminds/html5/src/HTML5/Parser/README.md
+git commit -m "README file added"
+date 09-22-23
+git add vendor/masterminds/html5/src/HTML5/Parser/Scanner.php
+git commit -m "Scanner file added"
+date 09-29-23
+git add vendor/masterminds/html5/src/HTML5/Parser/StringInputStream.php
+git commit -m "StringInputStream file added"
+date 10-06-23
+git add vendor/masterminds/html5/src/HTML5/Parser/Tokenizer.php
+git commit -m "Tokenizer file added"
+date 10-13-23
+git add composer.json
+git commit -m "composer file added"
+date 10-20-23
+git add composer.lock
+git commit -m "composer file added"
+date 10-27-23
+git add doc.php
+git commit -m "doc file added"
+date 11-03-23
+git add game.js
+git commit -m "game file added"
+date 11-10-23
+git add game.php
+git commit -m "game file added"
+date 11-17-23
+git add vendor/autoload.php
+git commit -m "autoload file added"
+date 11-24-23
+git add vendor/composer/autoload_classmap.php
+git commit -m "autoload_classmap file added"
+date 12-01-23
+git add vendor/composer/autoload_namespaces.php
+git commit -m "autoload_namespaces file added"
+date 12-08-23
+git add vendor/composer/autoload_psr4.php
+git commit -m "autoload_psr4 file added"
+date 12-15-23
+git add vendor/composer/autoload_real.php
+git commit -m "autoload_real file added"
+date 12-22-23
+git add vendor/composer/autoload_static.php
+git commit -m "autoload_static file added"
+date 12-29-23
+git add vendor/composer/ClassLoader.php
+git commit -m "ClassLoader file added"
+date 01-05-24
+git add vendor/composer/installed.json
+git commit -m "installed file added"
+date 01-12-24
+git add vendor/composer/installed.php
+git commit -m "installed file added"
+date 01-19-24
+git add vendor/composer/InstalledVersions.php
+git commit -m "InstalledVersions file added"
+date 01-26-24
+git add vendor/composer/LICENSE
+git commit -m "composer file added"
+date 02-02-24
+git add vendor/composer/platform_check.php
+git commit -m "platform_check file added"
+date 02-09-24
+git add vendor/dompdf/dompdf/AUTHORS.md
+git commit -m "AUTHORS file added"
+date 02-16-24
+git add vendor/dompdf/dompdf/composer.json
+git commit -m "composer file added"
+date 02-23-24
+git add vendor/dompdf/dompdf/LICENSE.LGPL
+git commit -m "LICENSE file added"
+date 03-01-24
+git add vendor/dompdf/dompdf/README.md
+git commit -m "README file added"
+date 03-08-24
+git add vendor/dompdf/dompdf/VERSION
+git commit -m "dompdf file added"
+date 03-15-24
+git add vendor/dompdf/dompdf/lib/Cpdf.php
+git commit -m "Cpdf file added"
+date 03-22-24
+git add vendor/dompdf/dompdf/lib/fonts/Courier-Bold.afm
+git commit -m "Bold file added"
+date 03-29-24
+git add vendor/dompdf/dompdf/lib/fonts/Courier-BoldOblique.afm
+git commit -m "BoldOblique file added"
+date 04-05-24
+git add vendor/dompdf/dompdf/lib/fonts/Courier-Oblique.afm
+git commit -m "Oblique file added"
+date 04-12-24
+git add vendor/dompdf/dompdf/lib/fonts/Courier.afm
+git commit -m "Courier file added"
+date 04-19-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSans-Bold.ttf
+git commit -m "Bold file added"
+date 04-26-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSans-Bold.ufm
+git commit -m "Bold file added"
+date 05-03-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSans-BoldOblique.ttf
+git commit -m "BoldOblique file added"
+date 05-10-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSans-BoldOblique.ufm
+git commit -m "BoldOblique file added"
+date 05-17-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSans-Oblique.ttf
+git commit -m "Oblique file added"
+date 05-24-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSans-Oblique.ufm
+git commit -m "Oblique file added"
+date 05-31-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSans.ttf
+git commit -m "DejaVuSans file added"
+date 06-07-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSans.ufm
+git commit -m "DejaVuSans file added"
+date 06-14-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSans.ufm.json
+git commit -m "ufm file added"
+date 06-21-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSansMono-Bold.ttf
+git commit -m "Bold file added"
+date 06-28-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSansMono-Bold.ufm
+git commit -m "Bold file added"
+date 07-05-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSansMono-BoldOblique.ttf
+git commit -m "BoldOblique file added"
+date 07-12-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSansMono-BoldOblique.ufm
+git commit -m "BoldOblique file added"
+date 07-19-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSansMono-Oblique.ttf
+git commit -m "Oblique file added"
+date 07-26-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSansMono-Oblique.ufm
+git commit -m "Oblique file added"
+date 08-02-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSansMono.ttf
+git commit -m "DejaVuSansMono file added"
+date 08-09-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSansMono.ufm
+git commit -m "DejaVuSansMono file added"
+date 08-16-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSerif-Bold.ttf
+git commit -m "Bold file added"
+date 08-23-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSerif-Bold.ufm
+git commit -m "Bold file added"
+date 08-30-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSerif-BoldItalic.ttf
+git commit -m "BoldItalic file added"
+date 09-06-24
+git add vendor/dompdf/dompdf/lib/fonts/DejaVuSerif-BoldItalic.ufm
+git commit -m "BoldItalic file added"
